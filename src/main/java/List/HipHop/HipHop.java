d7a66919b90075750fdbc03907d21dc35c1be0ff
progML <matveylesonen@gmail.com>
2024-02-09 10:42:46 +0300

    add solve from Hip Hop with iterator

--- src/main/java/List/HipHop/HipHop.java
+++ src/main/java/List/HipHop/HipHop.java
@@ -4,20 +4,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.stream.Collectors;
 
 public class HipHop {
     public static void main(String[] args) throws IOException, IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         List<String> list = Arrays.stream(reader.readLine().split("\\s")).collect(Collectors.toList());
-        for (String i : list) {
-            if (i.equals("Hip")) {
-                System.out.println(i);
-                System.out.println("Hop");
-            } else {
-                System.out.println(i);
-            }
+        ListIterator<String> listIterator = list.listIterator();
+        while (listIterator.hasNext()){
+            if (listIterator.next().equals("Hip"))
+                listIterator.add("Hop");
         }
+        list.forEach(System.out::println);
     }
 }
